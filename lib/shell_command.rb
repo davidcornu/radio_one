b5c99861fe6095040a7734f4f9e85a3bc78bd693
David Cornu <davidjcornu@gmail.com>
2015-09-26 16:03:48 -0400

    Add logging to ShellCommand

--- lib/shell_command.rb
+++ lib/shell_command.rb
@@ -1,20 +1,32 @@
+require 'logger'
 require 'open3'
 
 module ShellCommand
   class Error < StandardError; end
 
-  extend self
+  class << self
+    attr_writer :logger
 
-  def run!(*command)
-    stdout, stderr, status = Open3.capture3(*command)
-    unless status.success?
-      error_message = [
-        "Something went wrong",
-        "Command: #{command.join(' ')}",
-        stderr
-      ].join("\n")
-      raise Error, error_message
+    def logger
+      @logger ||= Logger.new("/dev/null")
     end
-    stdout
+
+    def run!(*command)
+      logger.debug("[ShellCommand] #{command.join(" ")}")
+
+      stdout, stderr, status = Open3.capture3(*command)
+      unless status.success?
+        error_message = [
+          "Something went wrong",
+          "Command: #{command.join(' ')}",
+          stderr
+        ].join("\n")
+        raise Error, error_message
+      end
+
+      logger.debug(stderr)
+
+      stdout
+    end
   end
 end
